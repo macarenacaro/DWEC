a00da53892aa06b83438acbc26c180621d217d65
Macarena Caro <maccarcor@alu.edu.gva.es>
2023-09-27 18:52:53 +0200

    Actividad 3 y 4

--- 3.ACTIVIDAD III/script.js
+++ 3.ACTIVIDAD III/script.js
@@ -5,4 +5,4 @@ let color = '#';
 for (let i = 0; i < 6; i++) //GENERAR NUMEROS ALEATORIOS
     color += letters[(Math.floor(Math.random() * 16))];
  
-document.getElementById("back").style.backgroundColor = color ;
+document.body.style.backgroundColor = color ;
---
+++ 4.ACTIVIDAD IV/script.js
@@ -0,0 +1,7 @@
+let numeros = prompt("Digita un número");
+
+if(parseInt(numeros)/parseInt(numeros)== 1){
+    alert('Has digitado un número');
+} else {
+    alert('NO has digitado un número');
+}
